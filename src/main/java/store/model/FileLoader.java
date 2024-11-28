edb33ad9d76652b7b90c40e90ad6d943ff27023c
kimsehw <seunghwan9703@gmail.com>
2024-11-28 14:16:23 +0900

    feat(FileLoader): 상품&프로모션 정보 불러오기 기능 구현

---
+++ src/main/java/store/model/FileLoader.java
@@ -0,0 +1,22 @@
+package store.model;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class FileLoader {
+
+    public static final String PRODUCTS_FILE = "src/main/resources/products.md";
+    public static final String PROMOTIONS_FILE = "src/main/resources/products.md";
+
+    public static void load(String filePath) {
+        List<String> FileData = null;
+        try {
+            FileData = Files.readAllLines(Path.of(filePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(FileData);
+    }
+}

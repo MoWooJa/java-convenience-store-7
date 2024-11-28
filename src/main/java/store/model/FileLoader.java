d1326954f73e1888d9fbdc97b9acba7c992fd44a
kimsehw <seunghwan9703@gmail.com>
2024-11-28 14:51:18 +0900

    refactor(FileLoader): 상품&프로모션 정보 반환하도록 수정

--- src/main/java/store/model/FileLoader.java
+++ src/main/java/store/model/FileLoader.java
@@ -10,13 +10,13 @@ public class FileLoader {
     public static final String PRODUCTS_FILE = "src/main/resources/products.md";
     public static final String PROMOTIONS_FILE = "src/main/resources/products.md";
 
-    public static void load(String filePath) {
+    public static List<String> load(String filePath) {
         List<String> FileData = null;
         try {
             FileData = Files.readAllLines(Path.of(filePath));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(FileData);
+        return FileData;
     }
 }
